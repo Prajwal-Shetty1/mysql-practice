5b2967dd824abb3ab0c0490ca36b4a96f165f3cd
Prajwal.S.Shetty <prajwalshetty7975@gmail.com>
2026-03-07 19:18:18 +0530

    SQL practice - Implemented SELECT, WHERE, LIMIT, ORDER BY, GROUP BY and aggregate functions.

---
+++ select_queries_practice.sql
@@ -0,0 +1,38 @@
+#SELECT in detail
+CREATE DATABASE abc;
+USE abc;
+CREATE TABLE student(
+  rollno INT,
+  name VARCHAR(50),
+  city VARCHAR(50)
+);
+INSERT INTO student VALUES
+(101,'Anil','Mangalore'),
+(102,'Rahul','Bangalore'),
+(103,'Priya','Mangalore');
+SELECT * FROM student;   #to select all values
+SELECT name,city FROM student; #to select particular column
+SELECT DISTINCT city FROM student; #stores unique value
+
+#WHERE CLAUSE
+SELECT * FROM STUDENT WHERE city="Mangalore";
+SELECT * FROM student WHERE name="Priya" and city="Mangalore";
+SELECT * FROM student WHERE city NOT IN ("Mangalore");
+
+#LIMIT CLAUSE
+SELECT * FROM student LIMIT 2;
+
+#GROUP BY CLAUSE-(asc and desc)
+SELECT * FROM student ORDER BY city ASC;
+SELECT * FROM student ORDER BY city DESC;
+
+#AGGREGATE FUNCTIONS-count(),max(),min(),sum(),avg()
+SELECT COUNT(*) FROM student;
+SELECT SUM(rollno) FROM student;
+SELECT MAX(rollno) FROM student;
+SELECT MIN(rollno) FROM student;
+SELECT AVG(rollno) FROM student;
+
+#GROUP BY CLAUSE
+SELECT city, COUNT(*) FROM student GROUP BY city;
+SELECT city, AVG(rollno) FROM student GROUP BY city;
